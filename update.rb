4ab9a1b7c7b4bdb2306655e497dbd7f1aef603aa
Jarod Luebbert <jarodluebbert@gmail.com>
2011-08-20 19:36:43 -0700

    working

---
+++ update.rb
@@ -0,0 +1,64 @@
+require 'rubygems'
+require 'nokogiri'
+require 'find'
+
+IMAGE_DIR = './images'
+CSS_DIR = './styles'
+JS_DIR = './scripts'
+INDEX = './index.html'
+
+image_files = []
+css_files = []
+js_files = []
+
+Find.find(IMAGE_DIR) do |f| image_files << f end
+Find.find(CSS_DIR) do |f| css_files << f end
+Find.find(JS_DIR) do |f| js_files << f end
+
+def filter_files(files, regex)
+  arr = []
+  files.each do |filename|
+    if filename =~ regex
+      arr << filename.gsub(/^.\//, '')
+    end
+  end
+  arr
+end
+
+images = filter_files(image_files, /.+.jpg/)
+styles = filter_files(css_files, /.+.css/)
+scripts = filter_files(js_files, /.+.js/)
+
+# build the doc
+builder = Nokogiri::HTML::Builder.new do |doc|
+  doc.html {
+    doc.head {
+      styles.each do |style|
+        doc.link(
+          :rel => "stylesheet",
+          :href => style,
+          :type => 'text/css',
+          :media => 'screen',
+          :charset => 'utf-8'
+        )
+      end
+      scripts.each do |script|
+        doc.script(
+          :type => 'text/javascript',
+          :charset => 'utf-8',
+          :src => script
+        )
+      end
+    }
+    doc.body {
+      images.each do |image|
+        doc.img(:src => image, :alt => image)
+      end
+    }
+  }
+end
+
+File.open(INDEX, 'w') do |file|
+  file.write(builder.to_html)
+end
+
