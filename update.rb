ebe21db9240978b75431935261aac44453a9560a
Jarod Luebbert <jarodluebbert@gmail.com>
2011-10-03 00:00:17 -0700

    added open graph type

--- update.rb
+++ update.rb
@@ -97,6 +97,7 @@ builder = Nokogiri::HTML::Builder.new do |doc|
       doc.meta(:name => 'google', :value => 'notranslate') {}
       doc.meta(:property => 'og:image', :content => 'http://jarodlphotos.s3.amazonaws.com/mailman.jpg') {}
       doc.meta(:property => 'fb:admins', :content => '505816573') {}
+      doc.meta(:property => 'og:type', :content => 'website') {}
       doc.title {
         doc.text 'Jarod Luebbert'
       }
