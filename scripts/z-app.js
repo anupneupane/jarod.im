fbca8685da79a312094986457ec2480966948c8a
Jarod Luebbert <jarodluebbert@gmail.com>
2011-10-11 23:39:25 -0700

    Added photo of a guy packing. yeah.

--- scripts/z-app.js
+++ scripts/z-app.js
@@ -1,6 +1,6 @@
 window.addEvent('load', function() {
   var ns = new NavSimple({
-    sections: '#2011-10-02-boating,#2011-10-01-mailman,#2011-10-01-dog-bed,#2011-09-30-guitar-player,#2011-09-17-waiting,#2011-09-11-cafe,#2011-09-11-blue-and-gold,#2011-09-05-reptile,#2011-09-03-buffalo,#2011-08-24-smitty,#2011-08-23-bug,#2011-08-16-flower,#2011-08-10-italian,#2011-08-10-guitarist,#2011-08-10-concert-girl,#2011-06-30-man-at-gg',
+    sections: '#2011-10-11-packing,#2011-10-02-boating,#2011-10-01-mailman,#2011-10-01-dog-bed,#2011-09-30-guitar-player,#2011-09-17-waiting,#2011-09-11-cafe,#2011-09-11-blue-and-gold,#2011-09-05-reptile,#2011-09-03-buffalo,#2011-08-24-smitty,#2011-08-23-bug,#2011-08-16-flower,#2011-08-10-italian,#2011-08-10-guitarist,#2011-08-10-concert-girl,#2011-06-30-man-at-gg',
     offset: { x: 0, y: 0 }
   });
   ns.activate();
