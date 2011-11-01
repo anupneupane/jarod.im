8a22c70a4ec8e4bbbb3e6be8a14ecf928c583925
Jarod Luebbert <jarodluebbert@gmail.com>
2011-11-01 01:46:17 -0700

    Added crosswalk photo

--- scripts/app.js
+++ scripts/app.js
@@ -23,7 +23,7 @@ function() {
 window.addEvent('load',
 function() {
     var ns = new NavSimple({
-        sections: '#2011-10-23-calm,#2011-10-23-break,#2011-10-16-spanish-guitarist,#2011-10-15-angel,#2011-10-11-packing,#2011-10-02-boating,#2011-10-01-mailman,#2011-10-01-dog-bed,#2011-09-30-guitar-player,#2011-09-17-waiting,#2011-09-11-cafe,#2011-09-11-blue-and-gold,#2011-09-05-reptile,#2011-09-03-buffalo,#2011-08-24-smitty,#2011-08-23-bug,#2011-08-16-flower,#2011-08-10-italian,#2011-08-10-guitarist,#2011-08-10-concert-girl,#2011-06-30-man-at-gg',
+        sections: '#2011-10-31-crosswalk,#2011-10-23-calm,#2011-10-23-break,#2011-10-16-spanish-guitarist,#2011-10-15-angel,#2011-10-11-packing,#2011-10-02-boating,#2011-10-01-mailman,#2011-10-01-dog-bed,#2011-09-30-guitar-player,#2011-09-17-waiting,#2011-09-11-cafe,#2011-09-11-blue-and-gold,#2011-09-05-reptile,#2011-09-03-buffalo,#2011-08-24-smitty,#2011-08-23-bug,#2011-08-16-flower,#2011-08-10-italian,#2011-08-10-guitarist,#2011-08-10-concert-girl,#2011-06-30-man-at-gg',
         offset: {
             x: 0,
             y: 0
