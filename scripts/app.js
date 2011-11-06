c407fe1e735e59f3c00da363a0612314f246d82b
Jarod Luebbert <jarodluebbert@gmail.com>
2011-11-06 00:18:33 -0700

    Minor site improvements.

--- scripts/app.js
+++ scripts/app.js
@@ -20,18 +20,23 @@ function() {
     resize();
 });
 
-window.addEvent('load',
-function() {
+window.addEvent('load', function() {
     var ns = new NavSimple({
-        sections: '#2011-10-31-crosswalk,#2011-10-23-calm,#2011-10-23-break,#2011-10-16-spanish-guitarist,#2011-10-15-angel,#2011-10-11-packing,#2011-10-02-boating,#2011-10-01-mailman,#2011-10-01-dog-bed,#2011-09-30-guitar-player,#2011-09-17-waiting,#2011-09-11-cafe,#2011-09-11-blue-and-gold,#2011-09-05-reptile,#2011-09-03-buffalo,#2011-08-24-smitty,#2011-08-23-bug,#2011-08-16-flower,#2011-08-10-italian,#2011-08-10-guitarist,#2011-08-10-concert-girl,#2011-06-30-man-at-gg',
+        sections: '.container>div',
         offset: {
             x: 0,
             y: 0
-        }
+        },
+        hashPathOnLoad: true
     });
     ns.activate();
-    ns.addEvent('scrollComplete',
-    function(section, curr, ns) {
-        window.location.hash = '#' + section.id;
+    var fadeBottom = function() {
+      $$('.bottom').set('tween', {duration: 10}).fade('out');
+    };
+    ns.addEvent('nextSection', fadeBottom);
+    ns.addEvent('previousSection', fadeBottom);
+    ns.addEvent('scrollComplete', function(section, curr, ns) {
+      window.location.hash = '#' + section.id;
+      $$('.bottom').set('tween', {duration: 1100}).fade('in');
     });
 });
